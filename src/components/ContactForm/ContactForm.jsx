6c829eb51a8de2ab450025460ac0e5521683806e
malaya1855 <121491813+malaya1855@users.noreply.github.com>
2023-07-13 10:59:32 +0100

    pretier

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,30 +1,25 @@
-import { Form, Input, ButtonForm } from "components";
+import { Form, Input, ButtonForm } from 'components';
 
-export const ContactForm = ({handleSubmit}) => {
-    return <Form  onSubmit={handleSubmit}>
-    <label htmlFor="name">
-        Name
-        </label>
-    <Input
-    type="text"
-    name="name"
-    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-    required
-    />
-  <label htmlFor="number">
-    Number
-    </label>
-  <Input
-  type="tel"
-  name="number"
-  pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-  title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-  required
-/>
-    <ButtonForm type="submit">
-      Add contact
-    </ButtonForm>
-  </Form>  
-}
-
+export const ContactForm = ({ handleSubmit }) => {
+  return (
+    <Form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name</label>
+      <Input
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
+      <label htmlFor="number">Number</label>
+      <Input
+        type="tel"
+        name="number"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+      />
+      <ButtonForm type="submit">Add contact</ButtonForm>
+    </Form>
+  );
+};
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,26 +1,38 @@
-import PropTypes from "prop-types";
-import { ButtonDelete, List } from "components";
+import PropTypes from 'prop-types';
+import { ButtonDelete, List } from 'components';
 
-export const ContactList = ({contacts, deleteContacts}) => {
-    return <ul>
-{contacts.map((contact) => {
-    const nameArray = contact.name.split(' ');
-    const nameFirstUpperLetter = nameArray.map((word) => 
-        word.replace(word[0], word[0].toUpperCase()))
-        .join(' ');
-return <List key={contact.id}>
-    <p>{nameFirstUpperLetter}: {contact.number}</p>
-    <ButtonDelete type='ButtonDelete' id= {contact.id} onClick = {deleteContacts}>Delete</ButtonDelete>
-    </List>
-    })}
-        </ul>
-}
+export const ContactList = ({ contacts, deleteContacts }) => {
+  return (
+    <ul>
+      {contacts.map(contact => {
+        const nameArray = contact.name.split(' ');
+        const nameFirstUpperLetter = nameArray
+          .map(word => word.replace(word[0], word[0].toUpperCase()))
+          .join(' ');
+        return (
+          <List key={contact.id}>
+            <p>
+              {nameFirstUpperLetter}: {contact.number}
+            </p>
+            <ButtonDelete
+              type="ButtonDelete"
+              id={contact.id}
+              onClick={deleteContacts}
+            >
+              Delete
+            </ButtonDelete>
+          </List>
+        );
+      })}
+    </ul>
+  );
+};
 ContactList.propTypes = {
-    contacts: PropTypes.arrayOf(
-        PropTypes.shape({
-          name: PropTypes.string.isRequired,
-          number: PropTypes.string.isRequired,
-          id: PropTypes.string.isRequired,
-        }).isRequired,
-      ).isRequired,
-  }
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+};
